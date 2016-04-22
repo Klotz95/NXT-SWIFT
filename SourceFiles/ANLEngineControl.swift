bd399b3ed724b9345daa2e9dce154b101fcc9050
Anton Wieprecht <antonwieprecht@Antons-MacBook-Pro-6.local>
2016-04-22 10:28:35 +0200

    ANLEngineContol.swift

--- SourceFiles/ANLEngineControl.swift
+++ SourceFiles/ANLEngineControl.swift
@@ -7,3 +7,29 @@
 //
 
 import Foundation
+
+class ANLEngineControl {
+    
+    // Variables
+    ANLBrickCommunication brick
+    
+    public func ANLEngineControl(currentBrick: ANLBrickCommunication) {
+        brick = currentBrick
+    }
+    
+    public func SetOutputState(outputPort: Byte, powerSetPoint: Byte, mode: Byte, regulationMode: UByte, turnRatio: SByte, runState: UByte, tachoLimit: ULong) -> NSError {
+        
+    }
+    
+    public func GetOutputState(outputPort: Byte) -> NSDictionary {
+        
+    }
+    
+    public func StopAllEngines() {
+        
+    }
+    
+    public func StopEngine() {
+        
+    }
+}
