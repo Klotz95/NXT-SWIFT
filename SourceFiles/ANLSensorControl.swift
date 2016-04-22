a1d2a2114844833ae58ba89897d013360e8a262f
Anton Wieprecht <antonwieprecht@Antons-MacBook-Pro-6.local>
2016-04-22 10:30:17 +0200

    added sensor control

---
+++ SourceFiles/ANLSensorControl.swift
@@ -0,0 +1,19 @@
+//
+//  ANLSensorControl.swift
+//  
+//
+//  Created by Anton Wieprecht on 22.04.16.
+//
+//
+
+import Foundation
+
+class ANLSensorControl {
+    
+    // Variables
+    ANLBrickCommunication brick
+    
+    public func GetInputValues(inputPort: Byte) -> NSDictionary {
+        
+    }
+}
